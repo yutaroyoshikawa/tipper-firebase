2f129b9a7a739dfdfa45fbd1c98fbe00256d9956
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2021-03-29 18:23:59 +0900

    eslint有効化

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,18 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+import { app } from "firebase-admin";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+  response.send("Hello from Firebase!");
 });
+
+export const onCreateUser = functions.handler.auth.user.onCreate(
+  (user, ctx) => {
+    app().auth().setCustomUserClaims(user.uid, {
+      userType: "user",
+    });
+    app().firestore().collection("users").doc(user.uid).create({});
+  }
+);
