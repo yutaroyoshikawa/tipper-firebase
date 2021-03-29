6daaae04902ae9f65130b5e19a70f146c5c4e7ed
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2021-03-29 20:44:29 +0900

    engineバージョン変更

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,11 +8,9 @@ export const helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
 
-export const onCreateUser = functions.handler.auth.user.onCreate(
-  (user, ctx) => {
-    app().auth().setCustomUserClaims(user.uid, {
-      userType: "user",
-    });
-    app().firestore().collection("users").doc(user.uid).create({});
-  }
-);
+export const onCreateUser = functions.handler.auth.user.onCreate((user) => {
+  app().auth().setCustomUserClaims(user.uid, {
+    userType: "user",
+  });
+  app().firestore().collection("users").doc(user.uid).create({});
+});
