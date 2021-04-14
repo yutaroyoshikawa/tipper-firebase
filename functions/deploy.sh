5acf301968fa2ea2790077da49d71ad854ad7601
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2021-04-14 21:27:17 +0900

    source修正

--- functions/deploy.sh
+++ functions/deploy.sh
@@ -1,19 +1,19 @@
 gcloud functions deploy helloWorld \
 --runtime nodejs12 \
 --timeout 540 \
---source lib \
+--source lib/index.js \
 --trigger-http
 
 gcloud functions deploy onCreateUser \
 --runtime nodejs12 \
 --timeout 540 \
---source lib \
+--source lib/index.js \
 --trigger-event providers/firebase.auth/eventTypes/user.create \
 --trigger-resource tipper-303209
 
 gcloud functions deploy mp4ToHls \
 --runtime nodejs12 \
 --timeout 540 \
---source lib \
+--source lib/index.js \
 --trigger-event google.storage.object.finalize \
 --trigger-resource /performances/videos
